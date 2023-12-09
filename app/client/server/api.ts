5731c3ad8ac01cda370cb14ca7924ec5e573d789
lcp <liucaoping@doodod.com>
2023-12-09 20:46:55 +0800

    feat(client): add http request

---
+++ app/client/server/api.ts
@@ -0,0 +1,44 @@
+interface RequestOptions {
+  method?: string;
+  data?: object;
+}
+
+interface RequestResult<T> {
+  onFinish: (data: T) => void;
+  onError?: (error: Error) => void;
+}
+
+export async function httpRequest<T>(
+  url: string,
+  options: RequestOptions,
+  callback: RequestResult<T>,
+) {
+  try {
+    const { method = "POST", data = {} } = options;
+    url = `http://localhost:8888${url}`;
+    const requestOptions: RequestInit = {
+      method: method.toUpperCase(),
+      body: method.toUpperCase() === "GET" ? undefined : JSON.stringify(data), // 如果是 GET 请求则不发送 body,
+    };
+    const token = localStorage.getItem("Authorization");
+    if (token != undefined) {
+      requestOptions.headers = {
+        Authorization: String(token),
+      };
+    }
+    const response = await fetch(url, requestOptions);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const resp = await response.json();
+    callback.onFinish(resp);
+  } catch (error) {
+    if (callback.onError) {
+      callback.onError(error as Error);
+    } else {
+      console.error("Error during HTTP request:", error);
+    }
+  }
+}
