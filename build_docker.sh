891908109d2690d41f80ed2c036ed344d6bb037e
liuyuhao <liuyuhao@doodod.com>
2024-01-02 22:32:14 +0800

    feat(url): since Alibaba Cloud registration failed, try Host  first V2

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,11 @@
 echo "building docker image next-chat:$1, login aliyun registry"
-cat .aliyun_pass | docker login --username=yanghuidong@yoooxin registry.cn-beijing.aliyuncs.com -u yanghuidong@yoooxin --password-stdin
-docker build -t next-chat:$1 .
+
+read -p "Enter your name: " username
+
+cat .aliyun_pass | docker login --username="$username"@yoooxin registry.cn-beijing.aliyuncs.com -u "$username"@yoooxin --password-stdin
+
+docker build --platform linux/amd64 -t next-chat:$1 .
+
 version=$1
 echo $version
 docker images | grep 'next-chat' | grep $version | awk '{print $3}' | while read image_id
